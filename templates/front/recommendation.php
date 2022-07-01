f69705f81a93a4305fe366183f23ac6cb270cdd3
ElenaKiseljova <e.a.kiseljova@gmail.com>
2022-07-01 11:33:34 +0300

    - поправила слайдеры

--- templates/front/recommendation.php
+++ templates/front/recommendation.php
@@ -9,32 +9,36 @@
       <h2 class="recommendation__title title"><?= $title; ?></h2>
 
       <?php if (!empty($list) && is_array($list) && !is_wp_error( $list )) : ?>
-        <div class="swiper recommendation__swiper">
-          <div class="swiper-wrapper">
-            <?php foreach ($list as $key => $item) : ?>
-              <?php 
-                $image = $item['image'] ? ($item['image']['sizes']['large'] ?? $item['image']['url'] ?? '') : '';
-                $alt = $item['image'] ? $item['image']['alt'] : 'recommendation';
-              ?>
-              <div class="swiper-slide recommendation__slide js-slide cursor-slide">
-                <img class="recommendation__img" src="<?= $image; ?>" alt="<?= $alt; ?>">
-              </div>
-            <?php endforeach; ?>          
+        <div class="furniture__cover">
+          <div class="swiper recommendation__swiper">
+            <div class="swiper-wrapper">
+              <?php foreach ($list as $key => $item) : ?>
+                <?php 
+                  $image = $item['image'] ? ($item['image']['sizes']['large'] ?? $item['image']['url'] ?? '') : '';
+                  $alt = $item['image'] ? $item['image']['alt'] : 'recommendation';
+                ?>
+                <div class="swiper-slide recommendation__slide js-slide cursor-slide">
+                  <img class="recommendation__img" src="<?= $image; ?>" alt="<?= $alt; ?>">
+                </div>
+              <?php endforeach; ?>          
+            </div>
           </div>
-        </div>
+        </div>        
 
         <div class="slider__wrapper slider__wrapper--mod">
-          <button class="slider__prev slider__prev--recommendation">
-            <svg class="slider__icon">
-              <use xlink:href="<?= get_template_directory_uri(  ); ?>/assets/img/sprite.svg#slider-prev"></use>
-            </svg>
+          <div class="slider__navigation">
+            <button class="slider__prev slider__prev--recommendation">
+              <svg class="slider__icon">
+                <use xlink:href="<?= get_template_directory_uri(  ); ?>/assets/img/sprite.svg#slider-prev"></use>
+              </svg>
 
-          </button>
-          <button class="slider__next slider__next--recommendation">
-            <svg class="slider__icon">
-              <use xlink:href="<?= get_template_directory_uri(  ); ?>/assets/img/sprite.svg#slider-next"></use>
-            </svg>
-          </button>
+            </button>
+            <button class="slider__next slider__next--recommendation">
+              <svg class="slider__icon">
+                <use xlink:href="<?= get_template_directory_uri(  ); ?>/assets/img/sprite.svg#slider-next"></use>
+              </svg>
+            </button>
+          </div>
 
           <div class="slider__pagination slider__pagination--recommendation"></div>
 
--- templates/product/simular.php
+++ templates/product/simular.php
@@ -16,48 +16,52 @@
     </div>
 
     <?php if (!empty($list) && is_array($list) && !is_wp_error( $list )) : ?>
-      <div class="swiper simular__swiper">
-        <!-- Additional required wrapper -->
-        <div class="swiper-wrapper">
-          <!-- Slides -->
-          <?php foreach ($list as $key => $item) : ?>
-            <div class="swiper-slide simular__slide">
-              <a href="<?= get_permalink( $item ); ?>" class="simular__link">
-                <?php if ( has_post_thumbnail( $item ) ) : ?>
-                  <?php 
-                    $thumbnail_desk = get_the_post_thumbnail_url( $item, 'large' ) ?? NOT_FOUND;
-                    $thumbnail_mobile = get_the_post_thumbnail_url( $item, 'medium' ) ?? NOT_FOUND;
-                  ?>
-                  <picture class="picture simular__picture">
-                    <source media="(min-width:768px)" srcset="<?= $thumbnail_desk; ?>">
+      <div class="simular__cover">
+        <div class="swiper simular__swiper">
+          <!-- Additional required wrapper -->
+          <div class="swiper-wrapper">
+            <!-- Slides -->
+            <?php foreach ($list as $key => $item) : ?>
+              <div class="swiper-slide simular__slide">
+                <a href="<?= get_permalink( $item ); ?>" class="simular__link">
+                  <?php if ( has_post_thumbnail( $item ) ) : ?>
+                    <?php 
+                      $thumbnail_desk = get_the_post_thumbnail_url( $item, 'large' ) ?? NOT_FOUND;
+                      $thumbnail_mobile = get_the_post_thumbnail_url( $item, 'medium' ) ?? NOT_FOUND;
+                    ?>
+                    <picture class="picture simular__picture">
+                      <source media="(min-width:768px)" srcset="<?= $thumbnail_desk; ?>">
 
-                    <img src="<?= $thumbnail_mobile; ?>" alt="<?= strip_tags( get_the_title( $item ) ?? '' ); ?>">
-                  </picture>
-                <?php else : ?>
-                  <picture class="picture simular__picture">
-                    <img src="<?= NOT_FOUND; ?>" alt="<?= strip_tags( get_the_title( $item ) ?? '' ); ?>">
-                  </picture>
-                <?php endif; ?>  
+                      <img src="<?= $thumbnail_mobile; ?>" alt="<?= strip_tags( get_the_title( $item ) ?? '' ); ?>">
+                    </picture>
+                  <?php else : ?>
+                    <picture class="picture simular__picture">
+                      <img src="<?= NOT_FOUND; ?>" alt="<?= strip_tags( get_the_title( $item ) ?? '' ); ?>">
+                    </picture>
+                  <?php endif; ?>  
 
-                <p class="simular__text"><?= get_the_title( $item ); ?></p>
-              </a>
-            </div>
-          <?php endforeach; ?>          
+                  <p class="simular__text"><?= get_the_title( $item ); ?></p>
+                </a>
+              </div>
+            <?php endforeach; ?>          
+          </div>
         </div>
-      </div>
+      </div>      
 
       <div class="slider__wrapper slider__wrapper--mod">
-        <button class="slider__prev slider__prev--simular">
-          <svg class="slider__icon">
-            <use xlink:href="<?= get_template_directory_uri(  ); ?>/assets/img/sprite.svg#slider-prev"></use>
-          </svg>
+        <div class="slider__navigation">
+          <button class="slider__prev slider__prev--simular">
+            <svg class="slider__icon">
+              <use xlink:href="<?= get_template_directory_uri(  ); ?>/assets/img/sprite.svg#slider-prev"></use>
+            </svg>
 
-        </button>
-        <button class="slider__next slider__next--simular">
-          <svg class="slider__icon">
-            <use xlink:href="<?= get_template_directory_uri(  ); ?>/assets/img/sprite.svg#slider-next"></use>
-          </svg>
-        </button>
+          </button>
+          <button class="slider__next slider__next--simular">
+            <svg class="slider__icon">
+              <use xlink:href="<?= get_template_directory_uri(  ); ?>/assets/img/sprite.svg#slider-next"></use>
+            </svg>
+          </button>
+        </div>
 
         <div class="slider__pagination slider__pagination--simular"></div>
 
